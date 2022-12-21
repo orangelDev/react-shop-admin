f2cf8c7acae9aafb3673e7c259acaa583a739d55
Orangel Brito <orangeleando@gmail.com>
2022-12-21 18:13:11 -0500

    Usando nuestro useAuth para cargar la info del usuario

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,4 +1,5 @@
 import { LockClosedIcon } from '@heroicons/react/24/outline';
+import { useRouter } from 'next/router';
 import { useRef } from 'react';
 import { useAuth } from 'hooks/useAuth';
 
@@ -6,6 +7,7 @@ export default function LoginPage() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
   const auth = useAuth();
+  const router = useRouter();
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -13,7 +15,7 @@ export default function LoginPage() {
     const password = passwordRef.current.value;
 
     auth.signIn(email, password).then(() => {
-      console.log('Login success');
+      router.push('/dashboard');
     });
   };
 
