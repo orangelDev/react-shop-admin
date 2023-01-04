22c6f6f6bf1776116029a0df3c4d57bc9ea73948
Orangel Brito <orangeleando@gmail.com>
2023-01-03 19:47:12 -0500

    Implementación del logout

--- src/pages/dashboard/products.js
+++ src/pages/dashboard/products.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
-import Modal from '@common/Modal';
 import { CheckIcon, XCircleIcon } from '@heroicons/react/24/solid';
-import { PlusIcon } from '@heroicons/react/24/solid';
+import Link from 'next/link';
+import Modal from '@common/Modal';
 import FormProduct from '@components/FormProduct';
 import axios from 'axios';
 import endPoints from '@services/api';
@@ -27,45 +27,37 @@ export default function Products() {
   }, [alert]);
 
   const handleDelete = (id) => {
-    deleteProduct(id)
-      .then(() => {
-        setAlert({
-          active: true,
-          message: 'Delete product succesfully',
-          type: 'success',
-          autoClose: false,
-        });
-      })
-      .catch((error) => {
-        setAlert({
-          active: true,
-          message: error.message,
-          type: 'error',
-          autoClose: false,
-        });
+    deleteProduct(id).then(() => {
+      setAlert({
+        active: true,
+        message: 'Delete product successfully',
+        type: 'error',
+        autoClose: true,
       });
+    });
   };
+
   return (
     <>
       <Alert alert={alert} handleClose={toggleAlert} />
       <div className="lg:flex lg:items-center lg:justify-between mb-8">
-        <div className="min-w-0 flex-1">
-          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">List of products</h2>
+        <div className="flex-1 min-w-0">
+          <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">List of Products</h2>
         </div>
         <div className="mt-5 flex lg:mt-0 lg:ml-4">
           <span className="sm:ml-3">
             <button
               type="button"
-              className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={() => setOpen(true)}
             >
               <CheckIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-              <PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
               Add Product
             </button>
           </span>
         </div>
       </div>
+
       <div className="flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -114,9 +106,8 @@ export default function Products() {
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{product.id}</td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <a href="/edit" className="text-indigo-600 hover:text-indigo-900">
-                          Edit
-                        </a>
+                        <Link href={`/dashboard/edit/${product.id}`} className="text-indigo-600 hover:text-indigo-900">Edit
+                        </Link>
                       </td>
                       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                         <XCircleIcon className="flex-shrink-0 h-6 w-6 text-gray-400 cursor-pointer" aria-hidden="true" onClick={() => handleDelete(product.id)} />
